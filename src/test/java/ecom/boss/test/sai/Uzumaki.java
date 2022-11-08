1b3d7fb411be7003aaaeb662e8ce26076881b453
GowdhamHatake <gowdhamandrewssingh@gmail.com>
2022-11-08 10:39:11 +0530

    sec push to pain

--- src/test/java/ecom/boss/test/sai/Uzumaki.java
+++ src/test/java/ecom/boss/test/sai/Uzumaki.java
@@ -1,5 +1,22 @@
 package ecom.boss.test.sai;
 
 public class Uzumaki {
+	
+public void one() {
+
+	System.out.println(2);
+}
+
+private void two() {
+
+	System.out.println(3);
+}
+
+private void three() {
+
+	System.out.println(4);
+}
+
+
 
 }
